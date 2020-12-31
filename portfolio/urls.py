88655a3632326d08239159d30ef448facc79dfa9
root <stephen>
2021-01-01 00:46:08 +0300

    added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,4 +24,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('' , include('steve.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
